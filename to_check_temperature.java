44c348ba294d534501a468348a239e6fbbee27a0
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:19:29 +0530

    Add files via upload

---
+++ to_check_temperature.java
@@ -0,0 +1,13 @@
+package ch3;
+
+public class to_check_temperature {
+
+	public static void main(String[] args) {
+		double temp= 40.5;
+		if (temp<=40.5);
+		System.out.println("it is nomal temperature");
+		
+
+	}
+
+}
