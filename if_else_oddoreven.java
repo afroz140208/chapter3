b1b59f0f22adbe01a7c570f0d3911f174ca94521
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:14:48 +0530

    Add files via upload

---
+++ if_else_oddoreven.java
@@ -0,0 +1,19 @@
+package ch3;
+
+public class if_else_oddoreven {
+
+	public static void main(String[] args) {
+		int number= 37;
+		if(number%2==0){
+			System.out.println("it is even number");
+			}
+			else {
+				System.out.println("it is odd number");
+			}
+		}
+		
+		
+		
+
+	}
+
