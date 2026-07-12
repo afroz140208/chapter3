c254d448248e537a6dcf7b0c7ac7296a0e4735d5
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:12:25 +0530

    Add files via upload

---
+++ greater_of_2nos.java
@@ -0,0 +1,17 @@
+package ch3;
+
+public class greater_of_2nos {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=40;
+		if(a>=b) {
+			System.out.println("a is smaller");
+		}
+		else {
+			System.out.println("b is greater");
+		}
+
+	}
+
+}
