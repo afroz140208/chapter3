f8809c45fa1d7c1ce1f5e4dca9542f2dcbb80939
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:19:59 +0530

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,25 @@
+package ch3;
+
+public class Vehicle {
+
+	public static void main(String[] args) {
+	int type=1;
+		System.out.println("1.bike");
+		System.out.println("2.auto");
+		System.out.println("3.car");
+		System.out.println("4.truck");
+switch(type) {
+case 1 : System.out.println("you choose bike");
+case 2 : System.out.println("you choose auto");
+case 3 : System.out.println("you choose bike");
+case 4 : System.out.println("you choose truck");
+default: System.out.println("invalid type");
+
+
+
+
+
+}
+	}
+
+}
