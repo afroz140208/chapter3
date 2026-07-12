7a6e170970b09d380edb9074d7b480c6a35926a8
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:18:49 +0530

    Add files via upload

---
+++ student_result.java
@@ -0,0 +1,13 @@
+package ch3;
+
+public class student_result {
+
+	public static void main(String[] args) {
+		int mark=80;
+		if(mark>=80);
+		System.out.println("student is passed");
+		
+
+	}
+
+}
