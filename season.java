51c4941e95961fd59a3f9a0a27b34daabd156a30
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:17:54 +0530

    Add files via upload

---
+++ season.java
@@ -0,0 +1,21 @@
+package ch3;
+
+public class season {
+
+	public static void main(String[] args) {
+		int season = 4;
+		switch (season) {
+		case 1 : System.out.println("summer");
+		break;
+		case 2 : System.out.println("winter");
+		break;
+		case 3 : System.out.println("autumn");
+		break;
+		case 4: System.out.println("moonsoon");
+		break;
+		default:System.out.println("invalid day");
+		}
+
+	}
+
+}
