5e96a01864ef519dac7f869ae6da18fbde8f8b97
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:19:15 +0530

    Add files via upload

---
+++ temperature.java
@@ -0,0 +1,19 @@
+package ch3;
+
+public class temperature {
+
+	public static void main(String[] args) {
+		int temp = 45;
+		if(temp>45) {
+			System.out.println("temp is high");
+		}
+		else if(temp<=45) {
+			System.out.println("temp is moderate");
+		}
+		else {
+			System.out.println("temp is low");
+		}
+
+	}
+
+}
