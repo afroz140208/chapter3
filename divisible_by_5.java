dde77508e4a57bb6f1cfe9959d406f2f3e237b3a
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:09:48 +0530

    Add files via upload

---
+++ divisible_by_5.java
@@ -0,0 +1,13 @@
+package ch3;
+
+public class divisible_by_5 {
+
+	public static void main(String[] args) {
+		int num=50;
+		if(num%5==0);
+		System.out.println("it is deisible by 5");
+		
+
+	}
+
+}
