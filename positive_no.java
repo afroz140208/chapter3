cde02df8edd30fe49c054a82fecdee9b9ec0bab7
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:17:09 +0530

    Add files via upload

---
+++ positive_no.java
@@ -0,0 +1,14 @@
+package ch3;
+
+public class positive_no {
+
+	public static void main(String[] args) {
+		int number=6;
+		if(number % 2==0);
+		System.out.println("it is positive");
+		
+	
+
+	}
+
+}
