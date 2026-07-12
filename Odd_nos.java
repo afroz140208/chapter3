e0b18be944b455868cc3da311de2643448fad8cc
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:16:32 +0530

    Add files via upload

---
+++ Odd_nos.java
@@ -0,0 +1,16 @@
+package ch3;
+
+public class Odd_nos {
+
+	public static void main(String[] args) {
+		int i=1;
+		for(i=1;i<=15;i++) {
+			if(i%2!=0) {
+				System.out.println("odd numbers:"+i);
+			}
+				
+		}
+
+	}
+
+}
