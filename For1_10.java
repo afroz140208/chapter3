db16215d047721dc0c11b1655ddb4b8e9cdb8418
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:11:52 +0530

    Add files via upload

---
+++ For1_10.java
@@ -0,0 +1,18 @@
+package ch3;
+
+public class For1_10 {
+
+	public static void main(String[] args) {
+		int i;
+		for( i = 1; i<=10; i++){
+			System.out.println("count:"+ i );
+			
+		}
+	
+			
+		}
+		
+
+	}
+
+
