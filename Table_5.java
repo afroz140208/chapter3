b0b14af632912637b4a03e0c9447ff27d38728ff
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:19:01 +0530

    Add files via upload

---
+++ Table_5.java
@@ -0,0 +1,14 @@
+package ch3;
+
+public class Table_5 {
+
+	public static void main(String[] args) {
+	int i=1;
+	
+		for( i=1; i<=10;i++) {
+			System.out.println("5*"+i+"="+(5*i));
+		}
+
+	}
+
+	}
