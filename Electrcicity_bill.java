9b3b71420f891431be8090fc8b1a753e9e2dd27c
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:10:05 +0530

    Add files via upload

---
+++ Electrcicity_bill.java
@@ -0,0 +1,27 @@
+package ch3;
+
+public class Electrcicity_bill {
+
+	public static void main(String[] args) {
+		int number=1;
+		System.out.println("welcome to tnpdcl");
+		System.out.println("1.acc no");
+		System.out.println("2.unit");
+		System.out.println("3.amount");
+		System.out.println("4.payment method");
+		switch(number) {
+		case 1: System.out.println("your acc no.11223");
+		case 2: System.out.println("total unit.600");
+		case 3: System.out.println("2880");
+		case 4: System.out.println("cash");
+		default:System.out.println("invalid acc no");
+		
+		
+		
+		
+		
+		}
+
+	}
+
+}
