2bfdb92a796c01536e903b142d550e6180cac9d8
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:20:44 +0530

    Add files via upload

---
+++ Vowels_consonants.java
@@ -0,0 +1,28 @@
+package ch3;
+
+public class Vowels_consonants {
+
+	public static void main(String[] args) {
+		char ch='b';
+		switch(Character.toLowerCase(ch)) {
+		case 'a':
+		case 'e':
+		case 'i':
+		case '0':
+		case 'u':
+			System.out.println("vowels");
+			default :
+			System.out.println("consonants");
+			}
+	
+		
+		
+		}
+		
+		
+		
+		
+
+	}
+
+
---
+++ votting.java
@@ -0,0 +1,18 @@
+package ch3;
+
+public class votting {
+
+	public static void main(String[] args) {
+		int age= 19;
+		if(age>=19) {
+		System.out.println("eligible to vote");
+		}
+		else {
+			System.out.println("not eligible to vote");
+		}
+		
+			
+
+	}
+
+}
