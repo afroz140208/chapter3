46e065247fa95fcf2462fcf10174484bf1212304
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:18:08 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,20 @@
+package ch3;
+
+public class Square {
+
+	public static void main(String[] args) {
+		int size=5;
+		
+		for(int i=1;i<=size;i++) {
+			for(int j=1;j<=size;j++) {
+				System.out.print("* ");
+				
+				
+			}
+			System.out.println();
+		}
+				
+
+	}
+
+}
