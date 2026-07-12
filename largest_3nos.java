d5cf0fdf2c635bbc3956cee5eae3cbe7a5148e13
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:15:41 +0530

    Add files via upload

---
+++ largest_3nos.java
@@ -0,0 +1,21 @@
+package ch3;
+
+public class largest_3nos {
+
+	public static void main(String[] args) {
+		int a=600;
+		int b=390;
+		int c=800;
+		if(a>=b&&a>=c) {
+			System.out.println("a is largest");
+			}
+		else if(b>=a&&b>=c) {
+			System.out.println("b is largest");
+		}
+		else {
+			System.out.println("c is largest");
+		}
+
+	}
+
+}
