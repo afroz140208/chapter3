b4a7202f97c684a296f2de1e5a22507e8925d253
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:17:25 +0530

    Add files via upload

---
+++ Repeated_triangle.java
@@ -0,0 +1,18 @@
+package ch3;
+
+public class Repeated_triangle {
+
+	public static void main(String[] args) {
+		int n=5;
+		
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
