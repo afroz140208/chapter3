4d36b0140f89ab0b098a51781d41737c6c785cfb
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:14:05 +0530

    Add files via upload

---
+++ Hallow_ryt_aligned_triangle.java
@@ -0,0 +1,29 @@
+package ch3;
+
+public class Hallow_ryt_aligned_triangle {
+
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=1;i<=n;i++) {
+			
+			
+		
+		for(int j=i;j<n;j++) {
+			System.out.print(" ");
+		
+	
+}
+		for(int j=1;j<=i;j++) {  
+			
+			
+			if(i==n||j==1||j==i) {
+				System.out.print("*");
+			}else {
+				System.out.print(" ");
+			}
+			
+		}
+		System.out.println();
+	}
+}
+}
