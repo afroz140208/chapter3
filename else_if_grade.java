ce4d7ae56c4fffb8f2003b4a2833db1743dcbdc9
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:10:18 +0530

    Add files via upload

---
+++ else_if_grade.java
@@ -0,0 +1,21 @@
+package ch3;
+
+public class else_if_grade {
+
+	public static void main(String[] args) {
+		int marks=70;
+		if(marks>=80) { 
+			System.out.println("grade a");
+		} else if (marks>=60) {
+			System.out.println("grade b");
+		}	else if(marks>=40) {
+			System.out.println("grade c");
+		} else {
+			System.out.println("fail");
+		}
+			
+		
+		
+	}
+}
+
