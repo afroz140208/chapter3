ea0eebdda62ea8b21c3c509b7771ca3db42140d5
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:16:57 +0530

    Add files via upload

---
+++ password.java
@@ -0,0 +1,18 @@
+package ch3;
+
+public class password {
+
+	public static void main(String[] args) {
+		String password="java";
+		if(password.equals("java")) {
+			System.out.println("it is correct");
+			}
+		else {
+			System.out.println("it is incorrect");
+			
+		}
+		
+
+	}
+
+}
