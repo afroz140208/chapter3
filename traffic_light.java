749aec2feedc8cf663e5747b6b52df346a5b0069
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:19:42 +0530

    Add files via upload

---
+++ traffic_light.java
@@ -0,0 +1,21 @@
+package ch3;
+
+public class traffic_light {
+
+	public static void main(String[] args) {
+		String colour = "red";
+		if(colour.equals("red")) {
+			System.out.println("stop");
+		}
+		else if(colour.equals("gren")){
+			System.out.println("go");
+			
+		}
+		else {
+			System.out.println("wait");
+		}
+		
+
+	}
+
+}
